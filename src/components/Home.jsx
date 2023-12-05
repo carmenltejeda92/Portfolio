19a53bf53d67feed290512430b77b3692fd160fc
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-12-05 13:26:15 -0500

    Attempting to create shooting animation - did not work

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,45 +1,45 @@
 import React from 'react'
 
 function Home() {
-
+    
 
 
   return (
-    <div className="board">
-            <div className="leftSide">
-                <div>
-                    <h1>Alien Ship</h1>
-                </div>
-                <div className="them">
-                    <div className='alien'><img src ="https://media.tenor.com/Re9FSRZKfN8AAAAi/mom-powerful.gif" alt="" 
-                        height='100px' width='100px'/></div>  
-                    <div className='alien'><img src ="https://media.tenor.com/Re9FSRZKfN8AAAAi/mom-powerful.gif" alt="" className="alien" 
-                        height='100px' width='100px'/></div>
-                    <div className='alien'><img src ="https://media.tenor.com/Re9FSRZKfN8AAAAi/mom-powerful.gif" alt="" className="alien" 
-                        height='100px' width='100px'/></div> 
-                    <div className='alien'><img src ="https://media.tenor.com/Re9FSRZKfN8AAAAi/mom-powerful.gif" alt="" className="alien" 
-                        height='100px' width='100px'/></div>   
-                </div>
-                <div className="alienStats">
-                    
-                </div>
-            </div>
-            <button className="shoot">Shoot</button>
-            <div className="rightSide">
-                <div>
-                    <h1>USS assembly</h1>
-                </div>
-                <div className="us">
-                    <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />
-                    <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />  
-                    <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />  
-                    <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />    
-                </div>
-                <div className="humanStats">
-                    
-                </div>
-            </div>
+    <div>
+        <div className='text'>
+            <h1>Alien Attack!!!</h1>
+            <p>Destroy the aliens to access my page, but be careful, they shoot back!</p>
         </div>
+        <div className="board">
+                <div className="leftSide">
+                    <div className="them">
+                        <div className='alien'><img src ="https://media.tenor.com/Re9FSRZKfN8AAAAi/mom-powerful.gif" alt="" 
+                            height='100px' width='100px'/></div>  
+                        <div className='alien'><img src ="https://media.tenor.com/Re9FSRZKfN8AAAAi/mom-powerful.gif" alt="" className="alien" 
+                            height='100px' width='100px'/></div>
+                        <div className='alien'><img src ="https://media.tenor.com/Re9FSRZKfN8AAAAi/mom-powerful.gif" alt="" className="alien" 
+                            height='100px' width='100px'/></div> 
+                        <div className='alien'><img src ="https://media.tenor.com/Re9FSRZKfN8AAAAi/mom-powerful.gif" alt="" className="alien" 
+                            height='100px' width='100px'/></div>   
+                    </div>
+                    <div className="alienStats">
+                        
+                    </div>
+                </div>
+                <button className="shoot">Shoot</button>
+                <div className="rightSide">
+                    <div className="us">
+                        <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />
+                        <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />  
+                        <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />  
+                        <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />    
+                    </div>
+                    <div className="humanStats">
+                        
+                    </div>
+                </div>
+            </div>
+    </div>
   )
 }
 
