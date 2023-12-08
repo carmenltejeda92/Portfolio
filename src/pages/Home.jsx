604362506e8d4a808aa5d04b77626c27c2ba4196
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-12-08 00:13:48 -0500

    Added labels

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,8 @@ import { useNavigate } from 'react-router-dom'
 import {useState} from 'react'
 import Ships from '../models/Ships'
 
-function Home() {
-    const [popUP, setPopUP] = useState(false)
-    const whileUP = popUP ? "while-UP" : ""
 
+function Home() {
     // document.addEventListener("keydown", this.keydown)
     // document.addEventListener("keyup", this.keyup)
     const nav = useNavigate()
@@ -20,13 +18,14 @@ function Home() {
 
     function shootThem(){
         let pOneTurn = true
+        const alienHull = document.querySelector('.lblAlienHull')
 
         let humanShips = 4
         let alienShips = 4
         if(pOneTurn){
             if(randomNum() > Ships[0].accuracy){
                 Ships[1].hull -= 5
-                console.log(Ships[1].hull)
+                alienHull.textContent = ('Aliens were hit! Their ships health is now: ' + Ships[1].hull)
                 if(Ships[1].hull === 0){
                     alienShips -= 1
                     console.log(alienShips)
@@ -55,14 +54,14 @@ function Home() {
 
 
   return (
-    <div className={'game' + whileUP}>
+    <div className='game'>
         <div className='text'>
             <h1>Alien Attack!!!</h1>
             <p>Destroy the aliens to access my page, but be careful, they shoot back!</p>
         </div>
         <div className="board">
                 <div className="leftSide">
-                    <div className={"them" + whileUP}>
+                    <div className="them">
                         <div id="alienOne" className='alien'><img src ="https://media.tenor.com/Re9FSRZKfN8AAAAi/mom-powerful.gif" alt="" 
                             height='100px' width='100px'/></div>  
                         <div className='alien'><img src ="https://media.tenor.com/Re9FSRZKfN8AAAAi/mom-powerful.gif" alt="" className="alien" 
@@ -73,18 +72,19 @@ function Home() {
                             height='100px' width='100px'/></div>   
                     </div>
                     <div className="alienStats">
+                        <label className='lblAlienHull'></label>
                     </div>
                 </div>
-                    <button id="shoot" className={whileUP} onClick={()=>shootThem() && setPopUP(true)}>Shoot</button>
+                    <button id="shoot" onClick={()=>shootThem()}>Shoot</button>
                 <div className="rightSide">
+                    <div className="humanStats">
+                        <label className='lblHumanHull'></label>
+                    </div>
                     <div className="us">
                         <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />
                         <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />  
                         <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />  
                         <img src ="https://media.tenor.com/jG3CfbW1f6oAAAAi/buccaneer-swashbuckler.gif" alt="" className="assembly" />    
-                    </div>
-                    <div className="humanStats">
-                        
                     </div>
                 </div>
         </div>
@@ -93,7 +93,6 @@ function Home() {
             <button className='btn' onClick={()=>nav('baseball')}>Fun Fact</button>
         </div>
         <div>
-            {popUP && <popUP setPopUP={setPopUP}/>}
         </div>
     </div>
   )
