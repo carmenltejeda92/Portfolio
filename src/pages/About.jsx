32c87b1263e7cd33632721382fa22eae8304a265
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-12-10 13:15:57 -0500

    Responsive layouts complete except for minor adjustments as it develops

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
     const nav = useNavigate()
    
   return (
-    <div className='aboutME'>
+    <div className='aboutME' >
       <div className='aboutMe'>
         <div>
           <img className='me' src='https://lh3.googleusercontent.com/pw/ADCreHes9aWZA7WcX2cd_nivqH8GaQkoJ94LdkxLWxg2yMdKdes6sfsiCSIrD3VY8Ur1rtACymFJFE6UQTHLj_mU9oiNzTuC8k79Uitqf8JdAjzgbSlQrIqg6gyyRDxFioAId_16lzT7nI-4zlw7SvLzospOPZX0JTBgsWGdebemO1YJTB8erQ6RBq5qzAXHgkZ8mMiFUf1FYfdolS6vi7L8UhhfS1hLVa_bpSBW1RY-ByTtjStn1GpfLzt3-gnwOY5WytllrzUo7pTekOuphCwXnip0zX9Rs8NGwU5Wt4ULbb9KZX_8sRoIwjvrRhUdq9oLqVxtd0Q-Ix4eNq8bJBgv-tq2z0dKY1vbVxqbyhjXt8d4XHBwg1JZwsBRym-7X1ULitb_7q3qSWMxEVZhTyFmW-4FBqYfZiBAfGx4oCIbwujX-laN0d9OeENUOyF_D1wToBWi2qzamJZg-4-WRuQ8Doxl0_6pK9zGJNmOF16quwshPOrcATsCGlNRufIyaqYfP4zo3e9PQNLkeB818cNufhW6gpsViOce7LtGBuRLIo4dDNTmAKDIBW4KUYVe0mXhIBzSJy9TALVhmO6KNer1AAhfQN4YjCNso6-BCWpGVNfVN7rbEbOr_YTd65rmu2-l1MAC0jd8zzBU1MCgA0XAM3KqhQfe_Yf3KjdhRQFfU7qfTkYV6B-FetAK5_ycfMrc_TaFejzFLbTktvH2hYFK2NTHTAp204NVOlyD6SnGhgj8jOHxv4R5EDRLcAq1Nm3Z30_QW9-bsXezfwtyj8vLTj0VzojRB7YbL4_Wt_9mk5H-LgM_xDtg9NuSoUzGcPqkzfAM5-LCKNeJDXk0YFqGWTVu7-tudrGtaEJL6yJSFHcABTjG1HuvdJGFZ40J2x2z86qTsIBvv29o-qTIiF4TEy67lEgMtTTSoNEvhwwJ_M1ZTkEHXHCNXTi2es39W_RXc42OyeOU3WM8Yg=w468-h873-s-no-gm?authuser=0'></img>
